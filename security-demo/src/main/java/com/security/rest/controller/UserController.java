0c7f3cc2862aa3fd67274600661d6c70cad947d1
kuanghaohua <951087952@qq.com>
2019-04-14 13:30:35 +0800

    获取用信息

--- security-demo/src/main/java/com/security/rest/controller/UserController.java
+++ security-demo/src/main/java/com/security/rest/controller/UserController.java
@@ -8,6 +8,10 @@ import org.apache.commons.lang.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +21,35 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    /**
+     * 返回用户登陆输入信息 + 用户的UserDetails信息即认证时的UserDetails
+     * @return
+     */
+    @GetMapping("/me1")
+    public Object getUser1(){
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    /**
+     * 同上
+     * @param authentication
+     * @return
+     */
+    @GetMapping("/me2")
+    public Object getUser2(Authentication authentication){
+        return authentication;
+    }
+
+    /**
+     * 只返回用户的UserDetails信息即认证时的UserDetails
+     * @param user
+     * @return
+     */
+    @GetMapping("/me3")
+    public Object getUser2(@AuthenticationPrincipal UserDetails user){
+        return user;
+    }
 
 
     @GetMapping
